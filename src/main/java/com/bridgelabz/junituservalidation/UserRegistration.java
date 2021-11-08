5a98e00eac2f32474904ac20ad4913641d49e698
PavaniKempula <pavanikempula1998@gmail.com>
2021-11-08 20:18:46 +0530

    ADDED : Welcome to User Registration Program using Juint Testing

---
+++ src/main/java/com/bridgelabz/junituservalidation/UserRegistration.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.junituservalidation;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        //Display Welcome Message
+        System.out.println("Welcome to User Registration Program using Junit Testing");
+    }
+}
